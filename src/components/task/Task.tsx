9506715e41d8843e9e402445ce7cecfa65c8688c
Irshath <irshath700@gmail.com>
2022-11-09 12:47:11 +0530

    chores: use link tag instead of onClick

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import TaskType from "@/models/Tasks";
 
 interface Props {
@@ -8,29 +8,20 @@ interface Props {
 
 export default function Task({ task, allStatus }: Props) {
 	const { _id, todoName, subTasks } = task;
-
-	const navigate = useNavigate();
 	const { board } = useParams();
 
 	const completedTask = subTasks.filter(
 		(subTask) => subTask.completed === true
 	).length;
 
-	const handleClick = (id: string) => {
-		navigate(`/${board}/update-task`, {
-			state: { task, allStatus },
-		});
-	};
-
 	return (
-		<section
-			onClick={() => handleClick(_id)}
-			className="my-4 w-60 cursor-pointer rounded-md bg-slate-100 p-4 backdrop-blur-sm dark:bg-zinc-800/50"
-		>
-			<h1 className="pb-2 text-slate-800  dark:text-slate-200">{todoName}</h1>
-			<p className="text-slate-700 shadow-sm dark:text-slate-400">
-				{completedTask} of {subTasks.length} substasks
-			</p>
-		</section>
+		<Link to={`/${board}/update-task`} state={{ task, allStatus }}>
+			<section className="my-4 w-60 cursor-pointer rounded-md bg-slate-100 p-4 backdrop-blur-sm dark:bg-zinc-800/50">
+				<h1 className="pb-2 text-slate-800  dark:text-slate-200">{todoName}</h1>
+				<p className="text-slate-700 shadow-sm dark:text-slate-400">
+					{completedTask} of {subTasks.length} substasks
+				</p>
+			</section>
+		</Link>
 	);
 }
