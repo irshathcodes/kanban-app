fb002d1366c21dd6a2832f7369ef6e5287e9efe3
Irshath <irshath700@gmail.com>
2022-11-22 09:32:55 +0530

    fix:change api base url

--- src/helpers/axios-instance.ts
+++ src/helpers/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://kanban-board-api.up.railway.app/api",
+	baseURL: "https://kanban-api-edr9.onrender.com/api",
 	withCredentials: true,
 });
 
