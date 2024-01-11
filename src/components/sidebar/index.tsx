f8cbdf135921d24744f35107a639493800678fd0
Irshath <irshath700@gmail.com>
2024-01-11 09:43:11 +0530

    change colors

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -26,14 +26,17 @@ export default function Sidebar() {
     },
   ];
   return (
-    <aside className="dark sticky h-screen bg-card text-card-foreground">
-      <div className="mt-4 flex items-center justify-between px-4">
-        <Link href="/" className="flex items-center gap-1 hover:opacity-80">
+    <aside className="sticky h-screen border-r border-r-border bg-card text-card-foreground">
+      <div className="mt-4 flex items-center justify-between">
+        <Link
+          href="/"
+          className="ml-4 flex items-center gap-1 hover:opacity-80"
+        >
           <KanbanSquare size={32} className="text-primary" />
           <h1 className="text-2xl font-bold">kanban</h1>
         </Link>
 
-        <Button size="icon" variant="ghost">
+        <Button className="mr-2" size="icon" variant="ghost">
           <PanelRightOpen />
         </Button>
       </div>
@@ -43,7 +46,7 @@ export default function Sidebar() {
           all boards (8)
         </h2>
 
-        <ul className="mt-5 space-y-4">
+        <ul className="mt-5 space-y-2">
           {boards.map((board) => {
             const link = `/${board.name}`;
             return (
