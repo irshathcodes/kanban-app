8f5c729e00c406c7730ef5726b8a6aebc5f74955
Irshath <irshath700@gmail.com>
2024-01-31 22:57:20 +0530

    add apis to get & create tasks

--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -1,24 +1,17 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { boards, columns, tasks } from "@/server/db/schema";
-import { eq, inArray, sql } from "drizzle-orm";
+import { columns, tasks } from "@/server/db/schema";
+import { eq, sql } from "drizzle-orm";
 import { z } from "zod";
 
 export const taskRouter = createTRPCRouter({
-  createBoard: protectedProcedure
-    .input(z.string())
+  createTask: protectedProcedure
+    .input(z.object({ name: z.string(), column_id: z.number() }))
     .mutation(async ({ input, ctx }) => {
-      const user_id = ctx.session.user.id;
-      const newBoard = await ctx.db
-        .insert(boards)
-        .values({ user_id, name: input });
-
-      const board_id = Number(newBoard.insertId);
-      await ctx.db.insert(columns).values([
-        { board_id, name: "todo", order: 0 },
-        { board_id, name: "doing", order: 1 },
-        { board_id, name: "done", order: 2 },
-      ]);
+      await ctx.db
+        .insert(tasks)
+        .values({ name: input.name, column_id: input.column_id });
     }),
+
   getColumnsWithTasks: protectedProcedure
     .input(z.number())
     .query(async ({ ctx, input }) => {
@@ -27,10 +20,12 @@ export const taskRouter = createTRPCRouter({
           column_id: columns.column_id,
           column_name: columns.name,
           column_order: columns.order,
-          tasks: sql`JSON_ARRAYAGG(JSON_OBJECT('task_id', ${tasks.task_id}, 'name', ${tasks.name}, 'description', ${tasks.description}))`,
+          tasks: sql<
+            Array<typeof tasks.$inferSelect>
+          >`JSON_ARRAYAGG(JSON_OBJECT('task_id', ${tasks.task_id}, 'name', ${tasks.name}, 'description', ${tasks.description}))`,
         })
         .from(tasks)
-        .leftJoin(columns, eq(tasks.column_id, columns.column_id))
+        .rightJoin(columns, eq(tasks.column_id, columns.column_id))
         .where(eq(columns.board_id, input))
         .groupBy(sql`${columns.column_id}`);
     }),
